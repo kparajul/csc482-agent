a745be22efb3af620d1486087f2c5e68d03069d2
Kritika Parajuli <124526898+kparajul@users.noreply.github.com>
2024-10-20 22:25:05 -0400

    imp stuff into env and dynamoDB writing

--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module main.go
 
 go 1.20
 
-require github.com/jamespearly/loggly v0.0.0-20220929110912-1d839b0715fa
+require (
+	github.com/aws/aws-sdk-go v1.55.5
+	github.com/jamespearly/loggly v0.0.0-20220929110912-1d839b0715fa
+	github.com/joho/godotenv v1.5.1
+)
 
-require github.com/TwiN/go-color v1.4.1 // indirect
+require (
+	github.com/TwiN/go-color v1.4.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	loggly "github.com/jamespearly/loggly"
+	"github.com/joho/godotenv"
 )
 
 type TotalResponse struct {
@@ -34,8 +41,14 @@ type RequiredResponse struct {
 }
 
 func main() {
-	client := loggly.New("My-Go-Demo")
-	ticker := time.NewTicker(5 * time.Minute)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	logglyToken := os.Getenv("LOGGLY_TOKEN")
+	client := loggly.New(logglyToken)
+	ticker := time.NewTicker(1 * time.Minute)
 	fetchData(client)
 
 	for range ticker.C {
@@ -45,7 +58,7 @@ func main() {
 
 func fetchData(client *loggly.ClientType) {
 	c := http.Client{Timeout: time.Duration(15) * time.Second}
-	req, err := http.NewRequest("GET", "http://www.reddit.com/r/all/comments.json?limit=100", nil)
+	req, err := http.NewRequest("GET", "http://www.reddit.com/r/all/comments.json?limit=20", nil)
 	if err != nil {
 		client.EchoSend("error", "error creating request")
 		return
@@ -70,13 +83,52 @@ func fetchData(client *loggly.ClientType) {
 	var response TotalResponse
 
 	err = json.Unmarshal(body, &response)
+
 	if err != nil {
 		client.EchoSend("error", "error decoding data")
 	}
+	size := len(response.Data.Children)
+
+	s := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(s)
+
+	tableName := "kparajul_Reddit_Comments"
+
+	client.EchoSend("info", fmt.Sprintf("Number of comments pulled: %d", size))
 	for _, child := range response.Data.Children {
 		comment := child.Data
-		logData := fmt.Sprintf("Fetched comments: Id: %s, author: %s, body: %s, score: %d", comment.Id, comment.Author, comment.Body, comment.Score)
-		client.EchoSend("info", logData)
+		fmt.Printf("\n Fetched comments: Id: %s, author: %s, body: %s, score: %d \n", comment.Id, comment.Author, comment.Body, comment.Score)
+
+		item := RequiredResponse{
+			Id:     comment.Id,
+			Author: comment.Author,
+			Body:   comment.Body,
+			Score:  comment.Score,
+		}
+
+		av, err := dynamodbattribute.MarshalMap(item)
+		if err != nil {
+			client.EchoSend("error", fmt.Sprintf("Marshalling error: %s", err))
+
+		}
+
+		input := &dynamodb.PutItemInput{
+			Item:      av,
+			TableName: aws.String(tableName),
+		}
+
+		_, err = svc.PutItem(input)
+
+		if err != nil {
+			client.EchoSend("error", fmt.Sprintf("Error during putItem %s", err))
+			return
+		}
+
+		client.EchoSend("info", "Successful adding to dynamoDB")
+
 	}
 
 }
